eb25af39287584ec3178e7bb0428b22512d054ec
Tyler McCown <tyler@astranis.com>
2025-03-26 13:30:58 -0700

    Macros for toggling checkmarks in markdown

--- .config/nvim/lua/core/editor.lua
+++ .config/nvim/lua/core/editor.lua
@@ -172,7 +172,32 @@ keymap.set('n', '<leader>ov', qfopen_wrapper('vs'), noremap)
 keymap.set('n', '<leader>ot', qfopen_wrapper('tabnew'), noremap)
 
 -- Command to split newline delimited raw strings
-vim.api.nvim_create_user_command('Splitlines', [[%s/\\n/\r/g]], {})
+api.nvim_create_user_command('Splitlines', [[%s/\\n/\r/g]], {})
 
 -- Command to print the outline of a python file
-vim.api.nvim_create_user_command('PyOutline', [[g/\v^\s{,4}(class|def)/p]], {})
+api.nvim_create_user_command('PyOutline', [[g/\v^\s{,4}(class|def)/p]], {})
+
+-- Checkmark toggling in markdown files
+local markdown_toggle_check = function()
+  local row, _ = unpack(api.nvim_win_get_cursor(0))
+  local current_line = api.nvim_get_current_line()
+  local new_line = ''
+  if string.find(current_line, '^%s*(- %[ %])') then
+    new_line = string.gsub(current_line, '%[ %]', '[x]', 1)
+  elseif string.find(current_line, '^%s*(- %[x%])') then
+    new_line = string.gsub(current_line, '%[x%]', '[ ]', 1)
+  else
+    local i, j = string.find(current_line, '^%s*')
+    assert(i ~= nil)
+    new_line = string.sub(current_line, i, j) .. string.gsub(current_line, '^%s*', '- [ ] ')
+  end
+  api.nvim_buf_set_lines(0, row - 1, row, true, { new_line })
+end
+api.nvim_create_autocmd('FileType', {
+  pattern = 'markdown',
+  callback = function()
+    api.nvim_create_user_command('MarkdownToggleCheck', markdown_toggle_check, {})
+
+    keymap.set('n', '<leader>l', ':MarkdownToggleCheck<cr>', silent_noremap)
+  end,
+})
