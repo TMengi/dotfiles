30c54b2ae01bac2fb11c253641d4a1f48c059823
Tyler McCown <tyler@astranis.com>
2025-03-29 17:18:30 -0700

    Editor reorganization

--- .config/nvim/lua/core/editor.lua
+++ .config/nvim/lua/core/editor.lua
@@ -6,7 +6,13 @@ local keymap = vim.keymap
 local opt = vim.opt
 local opt_local = vim.opt_local
 
--- Tab/indent behavior
+local silent_noremap = { silent = true, noremap = true }
+
+-- ============================================================================
+-- Basic editor behavior
+-- ============================================================================
+
+-- Tabs/indents
 opt.expandtab = true -- Use spaces instead of tabs
 opt.shiftwidth = 2 -- Shift n spaces on tab
 opt.tabstop = 2 -- 1 tab = n spaces
@@ -23,25 +29,11 @@ api.nvim_create_autocmd('FileType', {
 -- Search and highlighting
 opt.smartcase = true
 opt.hls = true
-keymap.set('n', '<leader>hh', ':set hls!<cr>', { silent = true })
+keymap.set('n', '<leader>hh', ':set hls!<cr>', silent_noremap)
 
 -- Split windows the way I expect them
 opt.splitbelow = true
 opt.splitright = true
-
--- Window navigation and resizing
-local silent_noremap = { silent = true, noremap = true }
-keymap.set('n', '<c-h>', '<c-w>h', silent_noremap)
-keymap.set('n', '<c-j>', '<c-w>j', silent_noremap)
-keymap.set('n', '<c-k>', '<c-w>k', silent_noremap)
-keymap.set('n', '<c-l>', '<c-w>l', silent_noremap)
-keymap.set('n', '<up>', ':resize +1<cr>', silent_noremap)
-keymap.set('n', '<down>', ':resize -1<cr>', silent_noremap)
-keymap.set('n', '<left>', ':vertical resize -1<cr>', silent_noremap)
-keymap.set('n', '<right>', ':vertical resize +1<cr>', silent_noremap)
-keymap.set('n', '<leader>wq', ':windo q<cr>', silent_noremap)
-keymap.set('n', '<leader>=', '<c-w>=', silent_noremap)
-keymap.set('n', '<leader>tn', ':tabnew<cr>', silent_noremap)
 
 -- Line numbering
 opt.number = true
@@ -69,59 +61,10 @@ api.nvim_create_autocmd({ 'InsertLeave' }, {
 -- Update faster than the default 4000 ms
 opt.updatetime = 100
 
--- Unconceal formatting characters
+-- Shortcut to unconceal formatting characters
 keymap.set('n', '<leader>cl', function()
   opt.conceallevel = 0
 end, silent_noremap)
-
--- Set the filetype for some uncommon extensions
-local buffer_events = { 'BufNewFile', 'BufEnter', 'BufRead' }
-local filetype_like = function(file_pattern, desired_filetype)
-  -- Sets the filetype for ``file_pattern`` files to be ``desired_filetype``
-  api.nvim_create_autocmd(buffer_events, {
-    pattern = file_pattern,
-    callback = function()
-      opt_local.filetype = desired_filetype
-    end,
-  })
-end
-filetype_like('BUILD.pants', 'pants')
-filetype_like('*.script', 'matlab') -- Pretend GMAT scripts are matlab
-filetype_like('*.prototxt', 'prototxt')
-filetype_like('*.sim', 'yaml')
-
--- Explicitly set syntax for certain uncommon filetypes
-local highlight_like = function(pattern, desired_syntax)
-  -- Highlights `pattern` files like they are `desired_syntax` files
-  api.nvim_create_autocmd(buffer_events, {
-    pattern = pattern,
-    callback = function()
-      opt_local.syntax = desired_syntax
-    end,
-  })
-end
-highlight_like('*.prototxt', 'yaml')
-highlight_like('BUILD.pants', 'python')
-highlight_like('.local_zshrc', 'zsh')
-
--- Explicitly set the comment string for certain uncommon filetypes
-local set_commentstring = function(filetype, commentstring)
-  api.nvim_create_autocmd('FileType', {
-    pattern = filetype,
-    callback = function()
-      opt_local.commentstring = commentstring .. ' %s'
-    end,
-  })
-end
-set_commentstring('pants', '#')
-set_commentstring('prototxt', '#')
-set_commentstring('kdl', '//')
-
--- Additive highlighting
-keymap.set('n', '<leader>*', 'viwy/<up>\\|\\<<c-r>0\\><cr>', silent_noremap)
-
--- Github view macro
-keymap.set('n', '<leader>v', ':!gv %<cr>', silent_noremap)
 
 -- Set commenting format options. Should be able to do this once with
 -- ops.formatoptions:remove({ 'tcro' }), but it seems like they get reset on
@@ -130,14 +73,36 @@ api.nvim_create_autocmd('FileType', {
   desc = 'Remote format options',
   command = 'set formatoptions=rqj',
 })
-
--- Search for merge conflicts
-keymap.set('n', '<leader>cf', '/<<<<<<<\\|=======\\|>>>>>>><cr>', { noremap = true })
-
 -- Autoreload changed buffers
 api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold', 'CursorHoldI' }, {
   command = "if mode() !~ '\v(c|r.?|!|t)' && getcmdwintype() == '' | checktime | endif",
 })
+
+-- ============================================================================
+-- Special commands and keymaps
+-- ============================================================================
+
+-- Window navigation and resizing
+keymap.set('n', '<c-h>', '<c-w>h', silent_noremap)
+keymap.set('n', '<c-j>', '<c-w>j', silent_noremap)
+keymap.set('n', '<c-k>', '<c-w>k', silent_noremap)
+keymap.set('n', '<c-l>', '<c-w>l', silent_noremap)
+keymap.set('n', '<up>', ':resize +1<cr>', silent_noremap)
+keymap.set('n', '<down>', ':resize -1<cr>', silent_noremap)
+keymap.set('n', '<left>', ':vertical resize -1<cr>', silent_noremap)
+keymap.set('n', '<right>', ':vertical resize +1<cr>', silent_noremap)
+keymap.set('n', '<leader>wq', ':windo q<cr>', silent_noremap)
+keymap.set('n', '<leader>=', '<c-w>=', silent_noremap)
+keymap.set('n', '<leader>tn', ':tabnew<cr>', silent_noremap)
+
+-- Additive highlighting
+keymap.set('n', '<leader>*', 'viwy/<up>\\|\\<<c-r>0\\><cr>', silent_noremap)
+
+-- Github view macro
+keymap.set('n', '<leader>v', ':!gv %<cr>', silent_noremap)
+
+-- Search for git merge conflicts
+keymap.set('n', '<leader>cf', '/<<<<<<<\\|=======\\|>>>>>>><cr>', { noremap = true })
 
 -- Quickfix list shortcuts
 local noremap = { noremap = true }
@@ -146,7 +111,6 @@ keymap.set('n', '<leader>cc', ':cclose<cr>', noremap)
 keymap.set('n', ']q', ':cnext<cr>', noremap)
 keymap.set('n', '[q', ':cprev<cr>', noremap)
 keymap.set('n', '<leader>cq', ':call setqflist([])<cr>', noremap)
-
 -- Functions and keymaps to open all quickfix items in various splits
 local qfopen_wrapper = function(...)
   local qfopen = function(cmd)
@@ -167,4 +131,4 @@ keymap.set('n', '<leader>ot', qfopen_wrapper('tabnew'), noremap)
 api.nvim_create_user_command('Splitlines', [[%s/\\n/\r/g]], {})
 
 -- Sort selected lines
-keymap.set('v', '<leader>s', ":sort<cr>", silent_noremap)
+keymap.set('v', '<leader>s', ':sort<cr>', silent_noremap)
