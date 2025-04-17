f077495d4cc0bef868bebd20d23869816b7bed07
Tyler McCown <tyler@astranis.com>
2025-04-16 17:55:05 -0700

    PyOutline qflist TODO

--- .config/nvim/lua/filetype_config/python.lua
+++ .config/nvim/lua/filetype_config/python.lua
@@ -4,6 +4,9 @@ local api = vim.api
 api.nvim_create_autocmd('FileType', {
   pattern = 'python',
   callback = function()
+    -- TODO: Can I turn this into qflist entries? Then I could open the results
+    -- in a picker and click through docstrings/jump to locations. That would
+    -- be so sick
     api.nvim_create_user_command('PyOutline', [[g/\v^\s{,4}(class|def)/p]], {})
   end,
 })
@@ -13,4 +16,3 @@ api.nvim_create_autocmd('FileType', {
   pattern = 'python',
   command = 'map <leader>y :tabnew pyrightconfig.json<cr>',
 })
-
