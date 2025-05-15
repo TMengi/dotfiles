20a8eacf5055237c06134386e312579615ab7a07
Tyler McCown <tyler@astranis.com>
2025-05-14 21:17:52 -0700

    Configure markdown toggle for current buffer only

--- .config/nvim/lua/filetype_config/markdown.lua
+++ .config/nvim/lua/filetype_config/markdown.lua
@@ -24,6 +24,6 @@ api.nvim_create_autocmd('FileType', {
   pattern = 'markdown',
   callback = function()
     api.nvim_create_user_command('MarkdownToggleCheck', markdown_toggle_check, {})
-    keymap.set('n', '<leader>ll', ':MarkdownToggleCheck<cr>', { silent = true, noremap = true })
+    keymap.set('n', '<leader>ll', ':MarkdownToggleCheck<cr>', { silent = true, noremap = true, buffer = true })
   end,
 })
