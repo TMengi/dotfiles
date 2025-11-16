43ccb6dbd1fb880b3e30a3ee357acef14dd52b87
tyler <tmccown21@gmail.com>
2025-11-15 23:00:53 -0800

    Remove leap repeat keys that were interfering with quickfix

--- .config/nvim/lua/plugin_config/leap.lua
+++ .config/nvim/lua/plugin_config/leap.lua
@@ -1,6 +1,3 @@
 local keymap = vim.keymap
 keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
 keymap.set('n',             'S', '<Plug>(leap-from-window)')
-
--- Set traversal keys to repeat previous motion
-require('leap.user').set_repeat_keys('<enter>', '<backspace>')
