b6b9b29b85d69af850fe52d2dedc6f9fb972a32a
tyler <tmccown21@gmail.com>
2026-05-08 16:20:44 -0700

    Add prettier formatter for yaml and ansible

--- .config/nvim/lua/plugin_config/formatter.lua
+++ .config/nvim/lua/plugin_config/formatter.lua
@@ -3,12 +3,26 @@ local keymap = vim.keymap
 local opts = { silent = true, noremap = true }
 keymap.set('n', '<leader>l', ':Format<cr>', opts) -- Format whole buffer
 
+-- The available configurations live in
+-- ~/.local/share/nvim/lazy/formatter.nvim/lua/formatter
 require('formatter').setup({
   logging = true,
   log_level = vim.log.levels.WARN,
   filetype = {
+    ['*'] = {
+      require('formatter.filetypes.any').remove_trailing_whitespace,
+    },
+    cpp = {
+      require('formatter.filetypes.cpp').clangformat,
+    },
+    json = {
+      require('formatter.filetypes.json').jq,
+    },
     lua = {
       require('formatter.filetypes.lua').stylua,
+    },
+    proto = {
+      require('formatter.filetypes.cpp').clangformat,
     },
     python = {
       require('formatter.filetypes.python').ruff,
@@ -16,23 +30,17 @@ require('formatter').setup({
     rust = {
       require('formatter.filetypes.rust').rustfmt,
     },
-    cpp = {
-      require('formatter.filetypes.cpp').clangformat,
-    },
-    proto = {
-      require('formatter.filetypes.cpp').clangformat,
-    },
-    json = {
-      require('formatter.filetypes.json').jq,
-    },
     sh = {
       require('formatter.filetypes.sh').shfmt,
     },
+    yaml = {
+      require('formatter.filetypes.yaml').prettier,
+    },
+    ['yaml.ansible'] = {
+      require('formatter.filetypes.yaml').prettier,
+    },
     zsh = {
       require('formatter.filetypes.sh').shfmt,
-    },
-    ['*'] = {
-      require('formatter.filetypes.any').remove_trailing_whitespace,
     },
   },
 })
