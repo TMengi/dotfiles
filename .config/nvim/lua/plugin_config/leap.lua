2aef86a2253dacce9b9b5f37a9eeee38460823a9
tmengi <tmccown21@gmail.com>
2026-06-20 22:19:29 -0700

    Remove leap repeat commands (#21)
    These repeat commands broke my ability to select things from the quickfix list. I tried to do some autocmd stuff to not bind these keys in the quickfixlist, but it didn't work because the quickfix filetype wasn't being set the first time you open the qflist in a session. Strange. Anyway I wasn't really using these so might as well get rid of them
    
    Co-authored-by: Tyler McCown <tyler@astranis.com>
    Reviewed-on: https://forgejo.tolur/tmengi/dotfiles/pulls/21

--- .config/nvim/lua/plugin_config/leap.lua
+++ .config/nvim/lua/plugin_config/leap.lua
@@ -1,19 +1,4 @@
-local leap = require('leap')
-local clever = require('leap.user').with_traversal_keys
+local keymap = vim.keymap
 
-vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
-vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
-
-vim.keymap.set({ 'n', 'x', 'o' }, '<cr>', function()
-  leap.leap({
-    ['repeat'] = true,
-    opts = clever('<cr>', '<bs>'),
-  })
-end)
-vim.keymap.set({ 'n', 'x', 'o' }, '<bs>', function()
-  leap.leap({
-    ['repeat'] = true,
-    opts = clever('<bs>', '<cr>'),
-    backward = true,
-  })
-end)
+keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
+keymap.set('n', 'S', '<Plug>(leap-from-window)')
