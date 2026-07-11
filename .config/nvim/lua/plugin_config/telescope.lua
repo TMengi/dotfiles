5122860f3e722bc24cb1b219a05b41624b42ed33
tyler <tmccown21@gmail.com>
2026-07-11 16:44:09 -0700

    Use telescope for history pickers, add ignore patterns (#23)
    Use telescope to open the search history and command history pickers
    
    Ignore some unnecessary file patterns in telescope fuzzy finding
    
    Co-authored-by: Tyler McCown <tyler@astranis.com>
    Reviewed-on: https://forgejo.tolur/tmengi/dotfiles/pulls/23

--- .config/nvim/lua/plugin_config/telescope.lua
+++ .config/nvim/lua/plugin_config/telescope.lua
@@ -1,5 +1,15 @@
 local keymap = vim.keymap
 
+local telescope = require('telescope')
+telescope.setup({
+  defaults = {
+    file_ignore_patterns = {
+      'vxworks_system',
+      'third_party',
+    },
+  },
+})
+
 local builtin = require('telescope.builtin')
 
 local opts = { noremap = true }
@@ -10,3 +20,7 @@ keymap.set('n', '<leader>fr', builtin.resume, opts)
 keymap.set('n', '<leader>fh', builtin.help_tags, opts)
 keymap.set('n', '<leader>gd', builtin.diagnostics, opts)
 keymap.set('n', '<leader>cto', builtin.quickfix, opts)
+
+-- Move the default history pickers into telescope
+keymap.set('n', 'q/', builtin.search_history, opts)
+keymap.set('n', 'q:', builtin.command_history, opts)
